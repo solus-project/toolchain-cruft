078335600f41426518d16609ea8acc371bf8ba6f
Ikey Doherty <ikey@solus-project.com>
2015-12-11 07:57:26 +0000

    Add patch
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- profiles/solus1_multilib/stage2/patch.sh
+++ profiles/solus1_multilib/stage2/patch.sh
@@ -20,25 +20,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-source "${SUBFILE}"
+# Always set PKG_NAME
+PKG_NAME="patch"
+PKG_URL="ftp://ftp.gnu.org/gnu/patch/patch-2.7.5.tar.xz"
+PKG_HASH="fd95153655d6b95567e623843a0e77b81612d502ecf78a489a4aed7867caa299"
 
-PACKAGES=(libstdc++ binutils gcc ncurses bash coreutils util-linux grep
-          sed tar bzip2 gzip xz diffutils patch)
+source "${FUNCTIONSFILE}"
 
-old_path="${PATH}"
-
-export PATH="/tools/bin:/tools/usr/bin:${PATH}"
-
-# We also have our own pre-requisites on the toolchain..
-export CONFIGURE_OPTIONS="--prefix=/tools "
-
-if [[ ! -d "${PKG_INSTALL_DIR}/tools" ]]; then
-    mkdir -p "${PKG_INSTALL_DIR}/tools" || do_fatal "Cannot create required tools directory"
-fi
-
-# Ensure we have a /tools/ symlink
-if [[ ! -e /tools/ ]]; then
-    sudo ln -sv "${PKG_INSTALL_DIR}/tools" /tools || do_fatal "Cannot create required /tools/ symlink"
-fi
-
-build_all
+# Now handle the arguments
+handle_args $*
