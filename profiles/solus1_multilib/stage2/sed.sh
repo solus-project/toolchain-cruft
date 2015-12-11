9c8be72940847730d5538e0e95a921432ba7b328
Ikey Doherty <ikey@solus-project.com>
2015-12-11 07:46:24 +0000

    Add sed
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- profiles/solus1_multilib/stage2/sed.sh
+++ profiles/solus1_multilib/stage2/sed.sh
@@ -20,24 +20,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-source "${SUBFILE}"
+# Always set PKG_NAME
+PKG_NAME="sed"
+PKG_URL="http://ftp.gnu.org/gnu/sed/sed-4.2.2.tar.gz"
+PKG_HASH="fea0a94d4b605894f3e2d5572e3f96e4413bcad3a085aae7367c2cf07908b2ff"
 
-PACKAGES=(libstdc++ binutils gcc ncurses bash coreutils util-linux grep sed)
+source "${FUNCTIONSFILE}"
 
-old_path="${PATH}"
-
-export PATH="/tools/bin:/tools/usr/bin:${PATH}"
-
-# We also have our own pre-requisites on the toolchain..
-export CONFIGURE_OPTIONS="--prefix=/tools "
-
-if [[ ! -d "${PKG_INSTALL_DIR}/tools" ]]; then
-    mkdir -p "${PKG_INSTALL_DIR}/tools" || do_fatal "Cannot create required tools directory"
-fi
-
-# Ensure we have a /tools/ symlink
-if [[ ! -e /tools/ ]]; then
-    sudo ln -sv "${PKG_INSTALL_DIR}/tools" /tools || do_fatal "Cannot create required /tools/ symlink"
-fi
-
-build_all
+# Now handle the arguments
+handle_args $*
