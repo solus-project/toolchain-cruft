c6d9c6fde14cb3fd70fec7de15c6d067272ba628
Ikey Doherty <ikey@solus-project.com>
2015-12-11 07:52:00 +0000

    Add gzip
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- profiles/solus1_multilib/stage2/gzip.sh
+++ profiles/solus1_multilib/stage2/gzip.sh
@@ -20,24 +20,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-source "${SUBFILE}"
+# Always set PKG_NAME
+PKG_NAME="gzip"
+PKG_URL="http://ftp.gnu.org/gnu/gzip/gzip-1.6.tar.xz"
+PKG_HASH="37dfed1a485d53212c43b3fa2a7c7952f09bf5cd86e37121c222341ee1b27847"
 
-PACKAGES=(libstdc++ binutils gcc ncurses bash coreutils util-linux grep sed tar bzip2 gzip)
+source "${FUNCTIONSFILE}"
 
-old_path="${PATH}"
-
-export PATH="/tools/bin:/tools/usr/bin:${PATH}"
-
-# We also have our own pre-requisites on the toolchain..
-export CONFIGURE_OPTIONS="--prefix=/tools "
-
-if [[ ! -d "${PKG_INSTALL_DIR}/tools" ]]; then
-    mkdir -p "${PKG_INSTALL_DIR}/tools" || do_fatal "Cannot create required tools directory"
-fi
-
-# Ensure we have a /tools/ symlink
-if [[ ! -e /tools/ ]]; then
-    sudo ln -sv "${PKG_INSTALL_DIR}/tools" /tools || do_fatal "Cannot create required /tools/ symlink"
-fi
-
-build_all
+# Now handle the arguments
+handle_args $*
